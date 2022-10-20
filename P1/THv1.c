609739ab14e5e9e3a5344e3faecc7072796ad1f6
UO CS Student <me@debian-BULLSEYE-live-builder-AMD64>
2022-10-20 16:03:09 -0700

    v1 start added

---
+++ P1/THv1.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "p1fxns.h"
+#include <errno.h>
+
+int int main(int argc, char const *argv[])
+{
+
+	
+	int i;
+	for(i = 0, i < nprocesses-1)
+	{
+		pid[i] = fork();
+		if
+	}
+	return 0;
+}
