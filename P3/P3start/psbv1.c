59001b95a9a185928a7593224d0c5222f8f656d9
Daniel Willard <dwillar4@uoregon.edu>
2022-11-20 16:57:35 -0800

    v1 almost complete

--- P3/P3start/psbv1.c
+++ P3/P3start/psbv1.c
@@ -1,6 +1,9 @@
 #include "BXP/bxp.h" // for bxp
 #include <pthread.h> // for p threads
 #include <assert.h> // for assert
+#include <time.h> //nanosleep
+#include <sys/time.h> // for time struct
+#include <signal.h> // sig handlers
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -10,15 +13,17 @@
 
 #define USAGE "Usage: ./psbv? [-f <filename>]"
 #define UNUSED __attribute__((unused))
-#define SERVICE "echo"
+#define SERVICE "PSB"
+#define HOST "localhost"
+#define PORT 19998
 
-volatile bool shutdown = false;
+volatile bool kill = false;
 
 //form lab 4 SIGINT handler
 static void onint(UNUSED int sig)
 {
 	printf("^C signal received, Shutting Down\n");
-	shutdown = true;
+	kill = true;
 }
 /* parses words form file.
 int extractwords(char *buf, char *sep, char *words[])
@@ -36,26 +41,24 @@ int extractwords(char *buf, char *sep, char *words[])
 */
 
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 	//varribles for BXP
-	BXPConnection bxp;
+	//BXPConnection bxp;
 	BXPEndpoint ep;
-	BxpService svc;
+	BXPService svc;
 	char channels[10000];
 	char query[10000];
 	char response[10001];
 	unsigned qlen;
 	unsigned rlen;
+	int port = PORT;
 	//varibles for file handling
 	FILE *fd = NULL;
 	int opt;
 	bool FFlag = false;
 	char buf[BUFSIZ];
-
-	int i = 0;
-
-
+	struct timespec ms20 = {0,20000000};
 
 	//step 1 open file and read then close
 	//Check the arguments provided; if “-f <filename>” use getopt
@@ -70,20 +73,20 @@ int main(int argc, char const *argv[])
 					if(optopt != 'f')
 						fprintf(stderr, "%s: illegal option '-%c'!\n", argv[0], optopt);
 					else
+					{
 						fprintf(stderr, "%s: illegal Usage.'-%c' [HERE] Requires an File Name \n", argv[0], optopt);
 						fprintf(stderr, "%s\n",USAGE);
+					}
 					goto cleanup;
 
 		}
 	}
-
-	
 	//error handling //
 	//Except for initialization failure, 
 	//open failure of the initialization file, and 
 	//receipt of an INT signal, PSBv1 should never exit.
 
-
+	signal(SIGINT,onint);
 	//open failure of the initialization file
 	if(FFlag)
 	{
@@ -98,36 +101,35 @@ int main(int argc, char const *argv[])
 
 	//<filename> contains aset of channel names, one per line
 	//open this file, read each channel name, and print “Creating publish/subscribe channel: %s\n”
-	
 	while(fgets(buf, BUFSIZ, fd) != NULL)
 	{
-		strcpy(channel, buf);
-		fprintf(stdout, "Creating publish/ subscride channel: %s\n", channel)
+		strcpy(channels, buf);
+		fprintf(stdout, "Creating publish/ subscride channel: %s\n", channels);
 	}
 	//after you have processed the entire file, you need to close it.
 	fclose(fd);
 
 	//Initialize the BXP runtime 1 so that it can create and accept encrypted connection requests
-	assert(bxp_init(0,1))
-	assert()
-
-
-
-
+	assert(bxp_init(port,1));
+	//form client.c example.
 	//Create a thread that will receive requests from client applications.
-
+	assert((svc = bxp_offer(SERVICE)));
 	//Respond to each such request by echoing back the received request along with a statusbyte; 
 	//the first byte of the response is ‘1’ for success, ‘0’ for failure
-
-	//the response to send back would be created by sprintf(response," 1%s " , query);
+	while((qlen = bxp_query(svc, &ep, query, 10000))> 0)
+	{
+		//the response to send back would be created by sprintf(response," 1%s " , query);
+		sprintf(response, "l%s", query);
+		rlen = strlen(response) +1;
+		assert(bxp_response(svc, &ep, response, rlen));
+	}
 
 	//wait for CTRL-C form lab 4
-	while(!shutdown)
+	while(!kill)
 	{
 		nanosleep(&ms20, NULL);
 	}
 	return EXIT_SUCCESS;
-
 
 	//cleanup close open file.
 	cleanup:
